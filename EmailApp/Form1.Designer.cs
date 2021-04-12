f2cad8b88c83ce8da062bb652b105c34a278d71c
chisoftltd <chisoftltd@gmail.com>
2021-04-12 19:42:39 +0200

    Add project files.

---
+++ EmailApp/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace EmailApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtEmailTo = new System.Windows.Forms.TextBox();
+            this.txtEmailFrom = new System.Windows.Forms.TextBox();
+            this.txtEmailSubject = new System.Windows.Forms.TextBox();
+            this.txtEmailBody = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.txtAttach = new System.Windows.Forms.TextBox();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.txtServerResponse = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblEmailList = new System.Windows.Forms.Label();
+            this.lblServerResponse = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(704, 439);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtAttach);
+            this.tabPage1.Controls.Add(this.btnAttach);
+            this.tabPage1.Controls.Add(this.btnSend);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.txtEmailBody);
+            this.tabPage1.Controls.Add(this.txtEmailSubject);
+            this.tabPage1.Controls.Add(this.txtEmailFrom);
+            this.tabPage1.Controls.Add(this.txtEmailTo);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(696, 413);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Send Emails";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblEmail);
+            this.tabPage2.Controls.Add(this.lblServerResponse);
+            this.tabPage2.Controls.Add(this.lblEmailList);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Controls.Add(this.txtEmail);
+            this.tabPage2.Controls.Add(this.txtServerResponse);
+            this.tabPage2.Controls.Add(this.btnQuit);
+            this.tabPage2.Controls.Add(this.btnDelete);
+            this.tabPage2.Controls.Add(this.btnConnect);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(696, 413);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Receive Emails";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtEmailTo
+            // 
+            this.txtEmailTo.Location = new System.Drawing.Point(105, 20);
+            this.txtEmailTo.Name = "txtEmailTo";
+            this.txtEmailTo.Size = new System.Drawing.Size(539, 20);
+            this.txtEmailTo.TabIndex = 0;
+            // 
+            // txtEmailFrom
+            // 
+            this.txtEmailFrom.Location = new System.Drawing.Point(105, 58);
+            this.txtEmailFrom.Name = "txtEmailFrom";
+            this.txtEmailFrom.Size = new System.Drawing.Size(539, 20);
+            this.txtEmailFrom.TabIndex = 1;
+            // 
+            // txtEmailSubject
+            // 
+            this.txtEmailSubject.Location = new System.Drawing.Point(105, 97);
+            this.txtEmailSubject.Name = "txtEmailSubject";
+            this.txtEmailSubject.Size = new System.Drawing.Size(539, 20);
+            this.txtEmailSubject.TabIndex = 2;
+            // 
+            // txtEmailBody
+            // 
+            this.txtEmailBody.Location = new System.Drawing.Point(105, 176);
+            this.txtEmailBody.Multiline = true;
+            this.txtEmailBody.Name = "txtEmailBody";
+            this.txtEmailBody.Size = new System.Drawing.Size(539, 153);
+            this.txtEmailBody.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FROM:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SUBJECT:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(296, 350);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(148, 46);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send Email";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAttach.Location = new System.Drawing.Point(26, 132);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(75, 23);
+            this.btnAttach.TabIndex = 8;
+            this.btnAttach.Text = "Attach";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+            // 
+            // txtAttach
+            // 
+            this.txtAttach.Location = new System.Drawing.Point(106, 132);
+            this.txtAttach.Name = "txtAttach";
+            this.txtAttach.Size = new System.Drawing.Size(538, 20);
+            this.txtAttach.TabIndex = 9;
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            this.openFD.Multiselect = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(579, 25);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(97, 36);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(579, 83);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 36);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.Location = new System.Drawing.Point(579, 152);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(97, 36);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // txtServerResponse
+            // 
+            this.txtServerResponse.Location = new System.Drawing.Point(17, 152);
+            this.txtServerResponse.Multiline = true;
+            this.txtServerResponse.Name = "txtServerResponse";
+            this.txtServerResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtServerResponse.Size = new System.Drawing.Size(535, 104);
+            this.txtServerResponse.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(17, 291);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEmail.Size = new System.Drawing.Size(535, 116);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(17, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(535, 95);
+            this.listBox1.TabIndex = 5;
+            // 
+            // lblEmailList
+            // 
+            this.lblEmailList.AutoSize = true;
+            this.lblEmailList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailList.Location = new System.Drawing.Point(17, 7);
+            this.lblEmailList.Name = "lblEmailList";
+            this.lblEmailList.Size = new System.Drawing.Size(75, 16);
+            this.lblEmailList.TabIndex = 6;
+            this.lblEmailList.Text = "Email List";
+            // 
+            // lblServerResponse
+            // 
+            this.lblServerResponse.AutoSize = true;
+            this.lblServerResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServerResponse.Location = new System.Drawing.Point(17, 133);
+            this.lblServerResponse.Name = "lblServerResponse";
+            this.lblServerResponse.Size = new System.Drawing.Size(116, 15);
+            this.lblServerResponse.TabIndex = 7;
+            this.lblServerResponse.Text = "Server Response";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(17, 272);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(47, 16);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 439);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EmailApplication";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmailBody;
+        private System.Windows.Forms.TextBox txtEmailSubject;
+        private System.Windows.Forms.TextBox txtEmailFrom;
+        private System.Windows.Forms.TextBox txtEmailTo;
+        private System.Windows.Forms.TextBox txtAttach;
+        private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblServerResponse;
+        private System.Windows.Forms.Label lblEmailList;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtServerResponse;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ EmailApp/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string emailTo { get; private set; }
+        public string emailFrom { get; private set; }
+        public string emailSubject { get; private set; }
+        public string emailBody { get; private set; }
+        public string userState { get; private set; }
+        public bool hasAttachment { get; private set; }
+        public DialogResult openFDResults { get; private set; }
+        public int numOfFiles { get; private set; }
+        public int counter { get; private set; }
+        public string[] aryAttachments { get; private set; }
+        public string popHost { get; private set; }
+        public string user { get; private set; }
+        public string pass { get; private set; }
+        public string returnString { get; private set; }
+        public NetworkStream networkStream { get; private set; }
+        public StreamReader readStream { get; private set; }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            emailTo = txtEmailTo.Text.Trim();
+            emailFrom = txtEmailFrom.Text.Trim();
+            emailSubject = txtEmailSubject.Text.Trim();
+            emailBody = txtEmailBody.Text.Trim();
+
+            SmtpClient smtpServer = new SmtpClient();
+
+            smtpServer.Host = "df-svr-dc03.dawnfresh.local";
+            smtpServer.Timeout = 60;
+            smtpServer.Port = 25;
+
+            smtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            smtpServer.UseDefaultCredentials = true;
+
+            MailMessage message = new MailMessage(emailFrom, emailTo);
+            message.Subject = emailSubject;
+            message.Body = emailBody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.Headers.Add("Reply-To", emailFrom);
+            message.Headers.Add("X-Organization", "Home and Learn");
+
+            if (hasAttachment)
+            {
+                for (int i = 0; i < aryAttachments.Length; i++)
+                {
+                    Attachment fileAttach = new Attachment(aryAttachments[i]);
+                    message.Attachments.Add(fileAttach);
+                }
+            }
+
+            smtpServer.SendCompleted += new SendCompletedEventHandler(DoSendCompleted);
+
+            userState = "- Mail Message";
+
+            try
+            {
+                smtpServer.SendAsync(message, userState);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DoSendCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            string token = e.UserState.ToString();
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Sending Email Cancelled " + token);
+            }
+            if (e.Error != null)
+            {
+                MessageBox.Show(token + e.Error.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Message Sent");
+            }
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            hasAttachment = false;
+
+            txtAttach.Text = "";
+
+            openFD.InitialDirectory = "C:\\";
+            openFD.Title = "Attach Files";
+            openFD.Filter = "PDFs|*.pdf|Text Files|*.txt|All Files|*.*";
+
+            openFDResults = openFD.ShowDialog();
+
+            if (openFDResults == DialogResult.Cancel)
+            {
+                hasAttachment = false;
+                return;
+            }
+
+            try
+            {
+                numOfFiles = openFD.FileNames.Length;
+                counter = 0;
+
+                aryAttachments = new string[numOfFiles];
+                foreach (string singleF in openFD.FileNames)
+                {
+                    aryAttachments[counter] = singleF;
+                    txtAttach.Text += singleF + "";
+                    counter++;
+
+                }
+                hasAttachment = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            TcpClient popServer = new TcpClient();
+            //IPAddress ipAddress = Dns.GetHostEntry("df-svr-dc03.dawnfresh.local").AddressList[0];
+            //IPAddress ipAddress = Dns.GetHostEntry("0.0.0.0").AddressList[0];
+            //IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 8080);
+            popHost = "df-svr-dc03.dawnfresh.local";
+            user = "Benjamin.chinwe@dawnfreshco.uk";
+            pass = "9thMile@9thMile@";
+
+            try
+            {
+                popServer.Connect("df-svr-dc03.dawnfresh.local", 8080);
+                networkStream = popServer.GetStream();
+                readStream = new StreamReader(networkStream);
+
+                returnString = readStream.ReadLine() + "\r\n";
+                txtServerResponse.Text = returnString;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
